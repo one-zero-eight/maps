48999fe5abcc995f56b103b819abbce1eb298b24
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-11-19 09:16:07 +0300

    feat: simplify start of development with cli

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -1,20 +1,77 @@
 import os
+import shutil
+import subprocess
 import sys
 from pathlib import Path
 
-import uvicorn
+BASE_DIR = Path(__file__).resolve().parents[2]
+os.chdir(BASE_DIR)
 
-# Change dir to project root (three levels up from this file)
-os.chdir(Path(__file__).parents[2])
+SETTINGS_TEMPLATE = BASE_DIR / "settings.example.yaml"
+SETTINGS_FILE = BASE_DIR / "settings.yaml"
+PRE_COMMIT_CONFIG = BASE_DIR / ".pre-commit-config.yaml"
+ACCOUNTS_TOKEN_URL = "https://api.innohassle.ru/accounts/v0/tokens/generate-service-token?sub=sports-local-dev&scopes=sport&only_for_me=true"
+
+
+def ensure_settings_file():
+    """
+    Ensure `settings.yaml` exists. If not, copy `settings.yaml.example`.
+    """
+    if not SETTINGS_TEMPLATE.exists():
+        print("❌ No `settings.yaml.example` found. Skipping copying.")
+        return
+
+    if SETTINGS_FILE.exists():
+        print("✅ `settings.yaml` already exists. Skipping copying.")
+        return
+
+    shutil.copy(SETTINGS_TEMPLATE, SETTINGS_FILE)
+    print(f"✅ Copied `{SETTINGS_TEMPLATE}` to `{SETTINGS_FILE}`")
+
+
+def ensure_pre_commit_hooks():
+    """
+    Ensure `pre-commit` hooks are installed.
+    """
+
+    def is_pre_commit_installed():
+        pre_commit_hook = BASE_DIR / ".git" / "hooks" / "pre-commit"
+        return pre_commit_hook.exists() and os.access(pre_commit_hook, os.X_OK)
+
+    if not PRE_COMMIT_CONFIG.exists():
+        print("❌ No `.pre-commit-config.yaml` found. Skipping pre-commit setup.")
+        return
+
+    if is_pre_commit_installed():
+        print("✅ Pre-commit hooks are already installed. Skipping pre-commit setup.")
+        return
+
+    try:
+        subprocess.run(
+            ["poetry", "run", "pre-commit", "install", "--install-hooks", "-t", "pre-commit", "-t", "commit-msg"],
+            check=True,
+            text=True,
+        )
+        print("✅ Pre-commit hooks installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Error setting up pre-commit hooks:\n{e.stderr}")
+        sys.exit(1)
+
+
+ensure_settings_file()
+ensure_pre_commit_hooks()
+
+import uvicorn  # noqa: E402
+
 # Get arguments from command
 args = sys.argv[1:]
+extended_args = [
+    "src.api.app:app",
+    "--use-colors",
+    "--proxy-headers",
+    "--forwarded-allow-ips=*",
+    *args,
+]
 
-uvicorn.main.main(
-    [
-        "src.api.app:app",
-        "--use-colors",
-        "--proxy-headers",
-        "--forwarded-allow-ips=*",
-        *args,
-    ]
-)
+print(f"🚀 Starting Uvicorn server: 'uvicorn {' '.join(extended_args)}'")
+uvicorn.main.main(extended_args)
